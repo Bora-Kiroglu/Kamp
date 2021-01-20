cbb4ca61f6800e04dbf0f36838b10ebd494030bf
Bora Kıroğlu <kartopu-2005@hotmail.com>
2021-01-20 23:50:38 +0300

    20.01.21 Ders Sonu

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product) 
+        {
+            Console.WriteLine("Ürün eklendi : " + product.ProductName);
+        }
+
+        public void Update(Product product) 
+        {
+            Console.WriteLine("Ürün güncellendi : " + product.ProductName);
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Sandalye";
+            product1.UnitPrice = 399.99;
+            product1.UnitsInStock = 49;
+
+            Product product2 = new Product{Id = 2, CategoryId = 5, UnitsInStock = 7, ProductName = "Kalem", UnitPrice = 7};
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+        }
+    }
+}
